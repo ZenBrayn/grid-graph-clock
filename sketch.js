1bb9800f92c60a4c9226b0a6cb7ee0068cfdb410
ZenBrayn <rwbenz@gmail.com>
2017-07-15 10:43:46 +0900

    Clock now updates size depending on display window size.

--- sketch.js
+++ sketch.js
@@ -1,26 +1,11 @@
 function setup() {  
-  // Using a fixed size for consistent display
-  // across devices
-  dwidth = 900;
-  dheight = 500;
-  createCanvas(dwidth, dheight);
-
+  createCanvas(windowWidth, windowHeight);
   background(0);
   noStroke();
   smooth();
-  
-  n_rows = 0;
-  n_cols = 0;
-  
-  margin_left = 100;
-  margin_top = 200;
-  
-  padding_x = 50;
-  spacing = 200;
 
   lbl_flag = false;
-
-  frameRate(4);
+  spacing = 0;
 }
 
 //--- Main drawing function
@@ -28,6 +13,14 @@ function draw() {
   // Refresh the background;
   background(0);
 
+  // Figure out the drawing dimensions
+  var margin_x = windowWidth * 0.10;
+  var margin_y = 50;
+  var padding = windowWidth * 0.10;
+  spacing = (windowWidth - 2 * margin_x - 2 * padding ) / 3;
+
+
+  // Get the time
   hr = hour();
   hr_str = String("00" + hr).slice(-2);
   mn = minute();
@@ -35,25 +28,33 @@ function draw() {
   sc = second();
   sc_str = String("00" + sc).slice(-2);
 
+  // draw the clock
   push()
   textAlign(LEFT, TOP);
-  textSize(72);
+  var fontsize1 = spacing * 0.50;
+  textSize(fontsize1);
   fill(255);
-  text("The time is...", margin_left * 0.9, 50);
-  textSize(18);
+  text("The time is...", margin_x * 0.9, margin_y);
+  var fontsize2 = spacing * 0.15;
+  textSize(fontsize2);
   fill(50);
-  text(hr_str + ":" + mn_str + ":" + sc_str, margin_left * 1.08, 125);
+  text(hr_str + ":" + mn_str + ":" + sc_str, margin_x * 1.08, margin_y + fontsize1);
   pop()
 
   // Grid graphs
-  translate(margin_left, margin_top)
+  clock_pos_y = 
+  translate(margin_x, margin_y + fontsize1 + fontsize2 + 75);
   drawGridGraphNo(hr, no_lbl = lbl_flag);
-  translate(spacing + padding_x, 0);
+  translate(spacing + padding, 0);
   drawGridGraphNo(mn, no_lbl = lbl_flag);
-  translate(spacing + padding_x, 0);
+  translate(spacing + padding, 0);
   drawGridGraphNo(sc, no_lbl = lbl_flag);
 }
 
 function mouseClicked() {
   lbl_flag = !lbl_flag;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
